8b6c09e0db4826fcc29c2313a3667ef322f27ef3
StackOverflow <96837517+stopperbir@users.noreply.github.com>
2024-09-02 23:52:00 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -36,7 +36,7 @@ key_frames_interval=$(echo `printf "%.1f\n" $(bc -l <<<"$key_frames_interval/10"
 key_frames_interval=${key_frames_interval%.*} # truncate to integer
 
 # static parameters that are similar for all renditions
-static_params="-c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0"
+static_params="-c:v copy -c:a aac -ar 44100 -ac 1"
 static_params+=" -g ${key_frames_interval} -keyint_min ${key_frames_interval} -hls_time ${segment_target_duration}"
 static_params+=" -hls_playlist_type vod"
 
